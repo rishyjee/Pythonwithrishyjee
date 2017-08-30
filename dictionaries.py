04bfdf473cb233bfc2421c479116d98f2a0b78f3
rishyjee <30544459+rishyjee@users.noreply.github.com>
2017-08-30 22:53:05 +0530

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -8,9 +8,10 @@ phone_books={
      'Vivek':['9773368681','viveky749@gmail.com','Male'],
      'Sandeep':['9920806773','sandyboxer@gmail.com','Male'],
      'Dad':['8652198983','surendraprasad123@gmail.com','Male'],
-     'Mayuri':['9845679720','mayuritiwari25@gmail.com','Female']
+     'Mayuri':['9845679720','mayuritiwari25@gmail.com','Female'],
+     'Amir':['9769486346','aamirkhan78692@gmail.com','Male']
             }
-print(phone_books['Mayuri'])
+print(phone_books['amir'])
 
 #Now I want to store multiple values associated with the particular key!!! What about the lists!!! shouldn't we use list!!! Isn't it bit or more interesting!!!
 #DICT[rishyjee]---> list of things containing phonenumber, emailid and his gender
